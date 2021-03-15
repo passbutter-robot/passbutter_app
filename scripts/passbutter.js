a6bd2c72dc94581bfa491ceb90872206d8d64dd5
Manuel Meyer <manuel.meyer.extern@googlemail.com>
2021-03-15 22:26:12 +0100

    fix ts.config for roslib.

--- scripts/passbutter.js
+++ scripts/passbutter.js
@@ -1,11 +1,21 @@
-import { Ros, Topic } from 'roslib';
-let ros = new Ros({});
-let example = new Topic({
+//import { Ros, Topic } from 'roslib';
+let ros = new ROSLIB.Ros({});
+ros.connect('ws://localhost:9090');
+ros.on('connection', function () {
+    console.log('Connected to websocket server.');
+});
+ros.on('error', function (error) {
+    console.log('Error connecting to websocket server: ', error);
+});
+ros.on('close', function () {
+    console.log('Connection to websocket server closed.');
+});
+var wheelSpeed = new ROSLIB.Topic({
     ros: ros,
     name: '/wheel_speed',
     messageType: 'std_msgs/Float32'
 });
-example.subscribe(function (message) {
+wheelSpeed.subscribe(function (message) {
     console.log(`Receive message: ${message}`);
 });
 //# sourceMappingURL=passbutter.js.map
--- scripts/passbutter.ts
+++ scripts/passbutter.ts
@@ -1,14 +1,29 @@
-import { Ros, Topic } from 'roslib';
+//import { Ros, Topic } from 'roslib';
 
-let ros = new Ros({
-})
-
-let example = new Topic({
-  ros : ros,
-  name : '/wheel_speed',
-  messageType : 'std_msgs/Float32'
+let ros = new ROSLIB.Ros({
+  
 });
 
-example.subscribe(function(message) {
-  console.log(`Receive message: ${message}`);
+ros.connect('ws://localhost:9090');
+
+ros.on('connection', function() {
+    console.log('Connected to websocket server.');
+});
+
+ros.on('error', function(error) {
+    console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function() {
+    console.log('Connection to websocket server closed.');
+});
+
+var wheelSpeed = new ROSLIB.Topic({
+    ros : ros,
+    name : '/wheel_speed',
+    messageType : 'std_msgs/Float32'
+});
+
+wheelSpeed.subscribe(function(message) {
+    console.log(`Receive message: ${message}`);
 });
